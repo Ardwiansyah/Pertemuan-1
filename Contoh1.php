a8574e37bf0da2a02a652b249eff32692a432905
Ardwiansyah <ardwiansyah230801@gmail.com>
2022-07-11 15:47:46 +0700

    Pertemuan 1

---
+++ Contoh1.php
@@ -0,0 +1,10 @@
+<?php  
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+      echo "Perkenalkan<br>";  
+      echo "Nama saya Muhammad Ardwiansyah. Saya seorang mahasiswa Universitas Bina Sarana Informatika.<br> ";
+      echo "Saya memiliki hobi bermain badminton.";
+    }
+}
